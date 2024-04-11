e8aea3219825768b7c0275ba7b55e5bac8cf8771
LIKELION-EUNTAEK <ahtp8155@naver.com>
2024-04-11 20:03:57 +0900

    first file

---
+++ src/main/java/likelion/helloworld/controller/Hello.java
@@ -0,0 +1,42 @@
+package likelion.helloworld.controller;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Hello {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+    @GetMapping("/hello-data")
+    public String helloData(Model model) {
+        model.addAttribute("nameKey", "은택 !!!");
+        return "hello";
+    }
+    @GetMapping("/hello/{name}/{age}")
+    public String helloPath(@PathVariable String name,
+                            @PathVariable String age,
+                            Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "hello";
+    }
+
+    @GetMapping("/hello-param")
+    public String helloParam(@RequestParam("이름") String name,
+                             @RequestParam("나이") String age,
+                             @RequestParam("학과") String major,
+                             @RequestParam("직업") String profess, Model model) {
+        model.addAttribute("이름", name);
+        model.addAttribute("나이", age);
+        model.addAttribute("학과", major);
+        model.addAttribute("직업", profess);
+        return "hello";
+    }
+}
