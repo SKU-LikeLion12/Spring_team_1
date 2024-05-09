5937a81baaea87038c69b28effedc0d093e3c170
Yithian01 <dbwlsdud10@naver.com>
2024-05-09 19:17:45 +0900

    2024/05/09_study

--- src/main/java/likelion/helloworld/repository/MemoryMemberRepository.java
+++ src/main/java/likelion/helloworld/repository/MemoryMemberRepository.java
@@ -1,12 +1,15 @@
 package likelion.helloworld.repository;
 
 import likelion.helloworld.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static final Map<Long, Member> local = new HashMap<>();
---
+++ src/main/java/likelion/helloworld/service/JwtUtility.java
@@ -0,0 +1,55 @@
+package likelion.helloworld.service;
+
+
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtUtility {
+
+    private String secret = "yourSecretKey";
+
+
+    private static final long EXPIRATION_TIME = 1000L * 60 * 60;
+
+
+    public String generateToken(String userId) {
+        return Jwts.builder()
+                .setSubject(userId)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, secret.getBytes(StandardCharsets.UTF_8))
+                .compact();
+
+    }
+
+
+    // BODY에 토큰 넣기
+    public Claims validateToken(String token) {
+        try{
+
+            Claims claims = Jwts.parser()
+                    .setSigningKey(secret.getBytes(StandardCharsets.UTF_8))
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            return claims;
+
+        } catch (SignatureException ex){
+
+            System.out.println("Invalid JWT signature");
+        }catch (ExpiredJwtException ex){
+
+            System.out.println("Expired JWT token");
+        }catch (Exception ex){
+
+            System.out.println("Invalid JWT token");
+        }
+
+        return null;
+    }
+
+}
